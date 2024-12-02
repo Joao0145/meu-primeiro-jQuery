da4dde9d7aba7baf25f0eea2d56e2b1f2de4aef0
João Victor Alves <160748355+Joao0145@users.noreply.github.com>
2024-12-02 09:21:37 -0300

    subir jQuery
    subir jQuery

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // Evento de clique no botao
+    $('#btnAlterar').click(function() {
+        // Alterar o texto do titulo
+        $('#titulo').text('Titulo Alterado com jQuery!');
+    });
+});
